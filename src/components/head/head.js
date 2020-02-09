344778f08f737d09e2e38be7fa4a554bcd91b52e
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2020-02-09 15:11:54 -0600

    We got Hotjar added

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -34,6 +34,18 @@ const Head = ({
         gtag('config', 'UA-158101812-1');
         `}
     </script>
+    <script>
+      {`
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1669848,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}
+    </script>
 
     <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
     <meta
