d66cbd3374850a8808b4df81d4d043d8941a38ff
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2020-02-09 20:57:22 -0600

    Fix. Right use of noscript for SSR

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React from 'react';  
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 import Head from 'components/head';
 import Header from 'components/header';
 import GlobalStyle from 'global.css.js';
 
+const Noscript = props => {
+  return <noscript dangerouslySetInnerHTML={{ __html: props.children }} />;
+};
+
 const Layout = ({ data, children }) => (
   <div>
     <GlobalStyle />
     <Head />
     <Header title={data.site.siteMetadata.siteTitle} />
-    {
-      // eslint-disable-next-line
-      <noscript><iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-M44KBB3"
-          height="0"
-          width="0"
-          style="display:none;visibility:hidden"
-        ></iframe>
-      </noscript>
-    }
+    <Noscript>
+      <iframe
+        title="googletagmanager"
+        src="https://www.googletagmanager.com/ns.html?id=GTM-M44KBB3"
+        height="0"
+        width="0"
+        style="display:none;visibility:hidden"
+      />
+    </Noscript>
     {children}
   </div>
 );
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Box from 'components/box';
 import Layout from 'components/layout';
+// eslint-disable-next-line
+const notFoundMessage = "All roads lead to Rome. We'll eventually get there, just not yet.";
 
 const NotFound = () => (
   <Layout>
-    <Box>{`All roads lead to Rome. We'll eventually get there, just not yet.`}</Box>
+    <Box>{notFoundMessage}</Box>
   </Layout>
 );
 
