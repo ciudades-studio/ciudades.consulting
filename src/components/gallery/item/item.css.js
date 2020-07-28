dc603c085ebd246c717d44fd6a13d4fba290a6e6
Josue Del Rio Gudiño <josue@ciudades.dev>
2020-07-28 00:17:10 -0500

    Created dynamic class for border radius

--- src/components/gallery/item/item.css.js
+++ src/components/gallery/item/item.css.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 import MEDIA from 'helpers/mediaTemplates';
+import GatsbyImage from 'gatsby-image';
 
 export const Title = styled.span`
   display: block;
   font-size: 2rem;
   font-weight: 500;
   margin: 2rem 2rem 1rem;
+`;
+
+export const RoundBorderImage = styled(GatsbyImage)`
+  border-radius: ${props => props.pixels}px;
 `;
 
 export const Copy = styled.p`
--- src/components/gallery/item/item.js
+++ src/components/gallery/item/item.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
-import { Title, Copy } from './item.css';
+import { Title, Copy, RoundBorderImage } from './item.css';
 
 const Item = ({ title, copy, image }) => (
   <figure>
-    <Img fluid={image ? image.childImageSharp.fluid : {}} alt={title} />
+    <RoundBorderImage fluid={image ? image.childImageSharp.fluid : {}} alt={title} pixels={13} />
     <figcaption>
       <Title>{title}</Title>
       <Copy>{copy}</Copy>
