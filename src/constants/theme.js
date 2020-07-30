c5f752486595efcfbd5be54ac682e2812619b292
Josue Del Rio Gudiño <josue@ciudades.dev>
2020-07-29 23:53:50 -0500

    Color pallet according to guideline

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,1 +1,37 @@
 export const accent = '#f90000';
+
+const colorPallet = {
+    primary: {
+       carraraGray :'#F0F0E8',
+       paleSkyGray: '#697984',
+    },
+    complementary: {
+        blues: {
+            faded: '#E2EDF2',
+            brightTurquoise: '#11CEED',
+            main: '#13A2EA',
+            bahamaBlue: '#0466A3',
+            veniceBlue: '#06497A',
+        },
+        reds: {
+            faded: '#F4B8B8',
+            froly: '#F97575',
+            main: '#EF5252',
+            alizarinCrimson: '#E52E2E',
+        },
+        oranges: {
+            faded: '#FFEDC0',
+            main: '#FFCE31',
+            tulipTree: '#E8B142',
+            neonCarrot: '#FFA233',
+        },
+        greens: {
+            faded: '#E1F2C0',
+            starship: '#D8ED3A',
+            main: '#94CC3B',
+            christi: '#75AA0F',
+        }
+    }
+};
+
+export { colorPallet };
