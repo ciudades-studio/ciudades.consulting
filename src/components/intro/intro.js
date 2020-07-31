22553f05a5f11daf032dca07d6315dd241005452
Josue Del Rio Gudiño <josue@ciudades.dev>
2020-07-31 01:25:24 -0500

    CDS-18 WIP

---
+++ src/components/intro/intro.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from '../../images/logo.svg';
+import './intro.css';
+
+const Intro = () => (
+  <div className="introContainer">
+      <div className="logoContainer"><Logo className="logo"/></div>
+  </div>
+);
+
+export default Intro;
