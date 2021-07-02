8e32f961f92bb561de3967e24be8a778dec9a51a
armanopbringas <bringas.armandop@gmail.com>
2021-07-02 00:00:26 -0500

    first steps to set up animation

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { motion, useViewportScroll } from 'framer-motion';
 import Logo from '../../images/logo.svg';
 import './intro.css';
 
-const Intro = () => (
-  <div className="intro">
-      <div className="intro__logo__container">
-        <Logo className="logo"/>
-      </div>
-  </div>
-);
+const Intro = () => {
+  const { scrollYProgress } = useViewportScroll();
+  return (
+    <div className="intro">
+      <motion.div
+        style={{ scaleX: scrollYProgress }}
+        className="intro__logo__container"
+      >
+        <Logo className="logo" />
+      </motion.div>
+    </div>
+  );
+};
 
 export default Intro;
