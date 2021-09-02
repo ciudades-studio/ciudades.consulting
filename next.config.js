9162b93e0553d1d13d0bdccd4180bbd2abd3c492
armanopbringas <bringas.armandop@gmail.com>
2021-09-02 10:19:30 -0500

    add configs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 const withSass = require('@zeit/next-sass');
+const assetRule = config.module.rules.find(({ test }) => test.test(".svg"));
+
+const assetLoader = {
+  loader: assetRule.loader,
+  options: assetRule.options || assetRule.query
+};
 
 module.exports = {
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      use: ["@svgr/webpack", assetLoader]
     });
 
     return config;
