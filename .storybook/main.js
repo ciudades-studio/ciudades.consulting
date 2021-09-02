0bf5222ae7dfa82a02c6745acb5b0fe57cb2e4ad
armanopbringas <bringas.armandop@gmail.com>
2021-09-02 10:18:37 -0500

    add configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,8 @@ module.exports = {
   stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    '@storybook/addon-links', 
+    '@storybook/addon-essentials'
   ],
 
   webpackFinal: async (baseConfig) => {
