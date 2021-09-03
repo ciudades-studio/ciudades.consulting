f666d98187ee82c3e0dbdf6d11d619948a322bbc
armanopbringas <bringas.armandop@gmail.com>
2021-09-03 17:50:04 -0500

    create fixture files

---
+++ fixtures/hero.fixture.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  title: "Services",
+  text: "We listen to your current needs, pain points during operations and highlights when it comes to processes. From idea to roadmap, from MVP to launch.",
+}
---
+++ fixtures/services.fixture.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+export interface IServices {
+  title: string
+  text?: string
+}
+
+export default {
+  title: "Services",
+  text: "We listen to your current needs, pain points during operations and highlights when it comes to processes. From idea to roadmap, from MVP to launch.",
+}
