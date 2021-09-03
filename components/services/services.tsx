f78cf0a742d9af9626038f8ed99a2a5bbaae7c8c
armanopbringas <bringas.armandop@gmail.com>
2021-09-03 17:48:47 -0500

    passing props

--- components/services/services.tsx
+++ components/services/services.tsx
@@ -1,17 +1,16 @@
+import { IServices } from "../../fixtures/services.fixture";
 import styles from "./srvices.module.scss";
  
-const Services = () => {
+const Services = ({ title="Services", text }: IServices) => {
   return (
     <div className={styles.services}>
       <div className={styles["services-wrapper"]}>
         <div className={styles["services-title-wrapper"]}>
-          <h2 className={styles["services-title"]}>\\ Services</h2>
+          <h2 className={styles["services-title"]}>\\ {title}</h2>
         </div>
         <div className={styles["services-text-wrapper"]}>
           <p className={styles["services-text"]}>
-            We listen to your current needs, pain points during 
-            operations and highlights when it comes to processes.From 
-            idea to roadmap, from MVP to launch.
+            {text}
           </p>
         </div>
       </div>
