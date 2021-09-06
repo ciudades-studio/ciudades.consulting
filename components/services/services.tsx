25ba431f0dc190f5cd4a4bf7d10358d926785619
armanopbringas <bringas.armandop@gmail.com>
2021-09-06 13:25:38 -0500

    add row for patterns and reducing the width for text wrapper and detailing heigh of elements

--- components/services/services.tsx
+++ components/services/services.tsx
@@ -1,18 +1,26 @@
 import { IServices } from "../../fixtures/services.fixture";
 import styles from "./srvices.module.scss";
  
-const Services = ({ title="Services", text }: IServices) => {
+const Services = ({ title="Services" }: IServices) => {
   return (
     <div className={styles.services}>
       <div className={styles["services-wrapper"]}>
         <div className={styles["services-title-wrapper"]}>
-          <h2 className={styles["services-title"]}>\\ {title}</h2>
+          <h2 className={styles["services-title"]}>
+            <span className={styles["services-title-span"]}>\\ {title}</span>
+          </h2>
         </div>
         <div className={styles["services-text-wrapper"]}>
           <p className={styles["services-text"]}>
-            {text}
+            We listen to your current needs, pain points during operations
+            and highlights when it comes to processes. From idea to roadmap, 
+            from MVP to launch.
           </p>
         </div>
+      </div>
+      <div className={styles.patterns}>
+        <div className={styles["second-patterns"]}/>
+        <div className={styles["third-patterns"]}/>
       </div>
     </div>
   );
