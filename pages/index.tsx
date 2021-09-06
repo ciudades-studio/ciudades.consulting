18a53af69e11a37b89371b51eb505e15eb090505
armanopbringas <bringas.armandop@gmail.com>
2021-09-06 13:27:39 -0500

    change dynamic content for static by now

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,8 @@ import Hero from "../components/hero/hero";
 import Services from "../components/services/services";
 import { IServices } from "../fixtures/services.fixture";
 
-const servText = "We listen to your current needs, pain points during operations and highlights when it comes to processes. From idea to roadmap, from MVP to launch."
 
-export default function Home({ title, text = servText }: IServices) {
+export default function Home({ title }: IServices) {
   return (
     <div>
       <Head>
@@ -15,7 +14,7 @@ export default function Home({ title, text = servText }: IServices) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
-      <Services title={title} text={text} />
+      <Services title={title} />
     </div>
   )
 }
