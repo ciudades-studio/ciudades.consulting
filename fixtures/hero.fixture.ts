99200daa4e9dc2dafedc7d40be56d0730ff2ec63
armanopbringas <bringas.armandop@gmail.com>
2021-09-07 23:37:57 -0500

    put the right content

--- fixtures/hero.fixture.ts
+++ fixtures/hero.fixture.ts
@@ -1,5 +1,8 @@
+export interface IHero {
+  title: string
+}
+
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  title: "Services",
-  text: "We listen to your current needs, pain points during operations and highlights when it comes to processes. From idea to roadmap, from MVP to launch.",
+  title: "Product & Software"
 }
