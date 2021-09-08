1838f4223a795476790fe06f490cffa8fabc85e0
armanopbringas <bringas.armandop@gmail.com>
2021-09-08 18:09:36 -0500

    put content

--- fixtures/industries.fixtures.ts
+++ fixtures/industries.fixtures.ts
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
 
 export interface IIndustries {
+  imgUrl?: string
   industriesTitle: string
   aboutText: string
   closingText: string
 }
 
 export default {
+  imgUrl: "https://s3.us-west-2.amazonaws.com/noble.holdings.site/aguascalientes.jpeg",
   industriesTitle: "Health Care",
   aboutText: "Privacy and data protection standards, user authentication and authorization, mass notifications and IoT data engineering ...",
   closingText: "We’ve been there."
