ed07c16921ff3d5c18ec857926529314dab35bf8
armanopbringas <bringas.armandop@gmail.com>
2021-09-08 14:03:58 -0500

    add new custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,21 @@ module.exports = {
     extend: {
       colors: {
         "main": "#f0f0e9",
-        "font-color": "#697984"
+        "font-color": "#697984",
+        "health-blue": "#13CEED"
       },
       height: {
-        "60vmin": "60vmin"
+        "60vmin": "60vmin",
+        "500": "500px"
       },
       width: {
-        "60vmin": "60vmin"
+        "60vmin": "60vmin",
+        "8xl": "90rem"
       }
     },
+    maxWidth: {
+      "title-industires-w": '100rem'
+    }
   },
   variants: {
     extend: {},
