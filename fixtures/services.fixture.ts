aabe0299b1e67db71560861131e011e33d09f6f3
armanopbringas <bringas.armandop@gmail.com>
2021-09-13 16:58:06 -0500

    add property for bg conditional classes

--- fixtures/services.fixture.ts
+++ fixtures/services.fixture.ts
@@ -2,21 +2,25 @@
 export interface IService {
   title: string
   text: string
+  pattern: "lines" | "dots-buildings"
 }
 
 const product = {
   title: "Services",
   text: "**Product**\n\nWe listen to your current needs, pain points during operations and highlights when it comes to processes.\n\nFrom idea to roadmap, from MVP to launch.",
+  pattern: "lines"
 }
 
 const software = {
   title: "Software",
   text: "**Applications and digital products**\n\nWe pick high quality and industry ready technologies according to each project and team.\n\nBoth consulting on architecture & best practices and projects get done.",
+  pattern: "lines"
 }
 
 const cloud = {
   title: "Cloud",
   text: "**Solutions and services AWS**\n\nAzure and sometimes GCP our choices.\n\nArchitecture best practices and costs saving our aim.\n\nData storage and migrations, tasks & processes scheduling, batching and workflows.\n\nWe enjoy it.",
+  pattern: "lines"
 }
 
 export default [ product, software, cloud ]
