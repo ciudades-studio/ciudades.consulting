5535075b923e54acc7690deaef8ab63317ad9039
armanopbringas <bringas.armandop@gmail.com>
2021-09-20 09:43:03 -0500

    iterate bullets array for search wrapp component

--- components/concept-component/concept-component.tsx
+++ components/concept-component/concept-component.tsx
@@ -1,14 +1,28 @@
+import { IConceptBullets } from "../../fixtures/concept-component.fixture";
+import bullets from "../../fixtures/concept-component.fixture";
 import styles from "./concept-component.module.scss";
+
+const ConceptWrapp = ({...bulletItems}: IConceptBullets) => {
+  return (
+    <div className={styles["concept-wrapp"]}>
+      <h2 className={styles["concept-text"]}>\\ {bulletItems.concept}:</h2>
+    </div>
+  );
+}
+
+const SearchtWrapp = ({...bulletItems}: IConceptBullets) => {
+  return (
+    <ul className={styles["search-wrapp"]}>
+      {bulletItems.bullets.map((bullet, index) => <li key={index} className={styles["search-text"]}>{bullet}</li>)}
+    </ul>
+  );
+}
 
 const ConceptComponent = () => {
   return (
     <div className={styles["concept-grid"]}>
-      <div className={styles["concept-wrapp"]}>
-        <h2 className={styles["concept-text"]}>\\ Concepto:</h2>
-      </div>
-      <ul className={styles["search-wrapp"]}>
-        <li className={styles["search-text"]}>Buscar</li>
-      </ul>
+      <ConceptWrapp concept={"Concept"} bullets={[]} />
+      <SearchtWrapp concept={""} bullets={["Bullet"]}  />
     </div>
   );
 }
