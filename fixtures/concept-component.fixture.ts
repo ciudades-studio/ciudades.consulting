5916d418705bdfdb63b1d5f01931433ba29a0036
armanopbringas <bringas.armandop@gmail.com>
2021-09-20 09:43:36 -0500

    add fixture files for concept component

---
+++ fixtures/concept-component.fixture.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export interface IConceptBullets {
+  concept: string,
+  bullets: string[]
+}
+
+const bulletItems = {
+  concept: "Concept",
+  bullets: ["Bullet"]
+}
+
+export default [ bulletItems ];
