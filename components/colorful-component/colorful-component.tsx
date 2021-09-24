2aed9ef916cd2039f65a2b692a2239c17ffd6768
armanopbringas <bringas.armandop@gmail.com>
2021-09-24 18:18:52 -0500

    simplify get bg functions

--- components/colorful-component/colorful-component.tsx
+++ components/colorful-component/colorful-component.tsx
@@ -1,42 +1,22 @@
 import cn from "classnames";
 import styles from "./colorful-component.module.scss";
+import colors from "../../fixtures/colorful-component.fixtures";
 import { IColorfulComponent } from "../../fixtures/colorful-component.fixtures";
 
 const ColorfulComponent = ({ textTitle, bgTitle, bgColor }: IColorfulComponent) => {
-
   const getBgColors = () => {
-    const blueBg = styles["blue-color"];
-    const greenBg = styles["green-color"];
-    const yellowBg = styles["yellow-color"];
+    const passColors = colors.map(() => styles[bgColor]);
     const mainWrapper = styles["main-wrapper"];
-    const hasBgBlue = bgColor === "blue-color";
-    const hasBgGreen = bgColor === "green-color";
-    const hasBgYellow = bgColor === "yellow-color";
-    const classes = cn(
-      mainWrapper, 
-      { [blueBg]: hasBgBlue }, 
-      { [greenBg]: hasBgGreen }, 
-      { [yellowBg]: hasBgYellow }
-    );
+    const classes = cn(mainWrapper, passColors);
     return classes;
-  };
+  }
 
   const getBgTitle = () => {
-    const blueBg = styles["blue-color"];
-    const greenBg = styles["green-color"];
-    const yellowBg = styles["yellow-color"];
+    const passColors = colors.map(() => styles[bgTitle]);
     const textWrapper = styles["text-wrapper"];
-    const hasBgBlue = bgTitle === "blue-color";
-    const hasBgGreen = bgTitle === "green-color";
-    const hasBgYellow = bgTitle === "yellow-color";
-    const classes = cn(
-      textWrapper, 
-      { [blueBg]: hasBgBlue }, 
-      { [greenBg]: hasBgGreen }, 
-      { [yellowBg]: hasBgYellow }
-    );
+    const classes = cn(textWrapper, passColors);
     return classes;
-  };
+  }
 
   return (
     <div className={getBgColors()}>
