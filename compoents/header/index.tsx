aa5b0607ad2b074726ca13754cec0de586d77bb7
armanopbringas <bringas.armandop@gmail.com>
2021-11-26 14:31:29 -0600

    layouting header

--- compoents/header/index.tsx
+++ compoents/header/index.tsx
@@ -1,12 +1,44 @@
-import styles from './header.module.css'
+import styles from './header.module.scss'
+
+const navItems = [
+  { item: "Services" },
+  { item: "Industries" },
+  { item: "About Us" },
+  { item: "Contact" }
+];
+
+const HeaderLogo = () => {
+  return(
+    <div className={styles["header-title-container"]}>
+      <h1 className={styles["header-title"]}>
+        Ciudades.dev
+      </h1>
+    </div>
+  );
+}
+
+const NavItems = () => {
+  return (
+    <nav className={styles["header-nav-container"]}>
+      <menu className={styles["header-nav-menu"]}>
+        {navItems.map(navItem => (
+          <span className={styles["menu-items"]}>
+            <a href="/">
+              {navItem.item}
+            </a>
+          </span>
+        ))}
+      </menu>
+    </nav>
+  );
+}
 
 const Header = () => {
   return (
-    <header>
-      <div className={styles.container}>
-        <div className={styles["big-screen-contanier"]}>
-          header
-        </div>  
+    <header className={styles["header-container"]}>
+      <div className={styles["container"]}>
+        <HeaderLogo />
+        <NavItems />
       </div>
     </header>
   );
