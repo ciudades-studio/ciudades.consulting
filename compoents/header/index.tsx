42c95a905dce59879437268ae0301fbb05b0a5d9
armanopbringas <bringas.armandop@gmail.com>
2021-11-30 21:59:46 -0600

    add index key in nav items component

--- compoents/header/index.tsx
+++ compoents/header/index.tsx
@@ -2,7 +2,7 @@ import styles from './header.module.scss'
 import { navItems } from '../../fixtures/header.fixtures';
 
 const HeaderLogo = () => {
-  return(
+  return (
     <div className={styles["header-title-container"]}>
       <h1 className={styles["header-title"]}>
         Ciudades.dev
@@ -15,8 +15,8 @@ const NavItems = () => {
   return (
     <nav className={styles["header-nav-container"]}>
       <menu className={styles["header-nav-menu"]}>
-        {navItems.map(navItem => (
-          <span className={styles["menu-items"]}>
+        {navItems.map((navItem, index) => (
+          <span key={index} className={styles["menu-items"]}>
             <a href="/">
               {navItem.item}
             </a>
