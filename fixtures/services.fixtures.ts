27d716febaabed585c362718d8bb37a293525404
armanopbringas <bringas.armandop@gmail.com>
2021-12-01 13:42:40 -0600

    create fixture content file for services component

---
+++ fixtures/services.fixtures.ts
@@ -0,0 +1,41 @@
+export interface IServices {
+  serviceTitle: string
+  serviceSubTitle: string
+  itemsDercription: []
+  imgServiceUrl: string
+}
+
+const digitalProducts = {
+  serviceTitle: "Digital Products Development",
+  serviceSubTitle: "We approach your project from strategies to implementation.",
+  itemsDercription: [
+    "Generate and publish attractive content to your market audiences.",
+    "Become publicly available on the Internet, reaching and engaging with your clients.",
+    "Be found at Google."
+  ],
+  imgServiceUrl: "../assets/services-assets/color_x5F_1.svg"
+}
+
+const iotDevelopment = {
+  serviceTitle: "IoT Development",
+  serviceSubTitle: "We approach your project from strategies to implementation.",
+  itemsDercription: [
+    "Generate and publish attractive content to your market audiences.",
+    "Become publicly available on the Internet, reaching and engaging with your clients.",
+    "Be found at Google."
+  ],
+  imgServiceUrl: "color_x5F_1 (1).svg"
+}
+
+const cloudAndArchitecture = {
+  serviceTitle: "Cloud Infrastructure & Software Architecture",
+  serviceSubTitle: "We approach your project from strategies to implementation.",
+  itemsDercription: [
+    "Generate and publish attractive content to your market audiences.",
+    "Become publicly available on the Internet, reaching and engaging with your clients.",
+    "Be found at Google."
+  ],
+  imgServiceUrl: "color_x5F_1 (2).svg"
+}
+
+export default [ digitalProducts, iotDevelopment, cloudAndArchitecture ];
