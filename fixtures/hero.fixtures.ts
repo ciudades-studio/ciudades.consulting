42eacfc877ff0fdd176f091a44ce06e989546be1
armanopbringas <bringas.armandop@gmail.com>
2021-12-04 00:23:45 -0600

    add data content

--- fixtures/hero.fixtures.ts
+++ fixtures/hero.fixtures.ts
@@ -3,7 +3,9 @@ export interface IHero {
   heroText: string
 }
 
-export default {
-  heroMainText: "string",
-  heroText: "string",
+const heroContent = {
+  heroMainText: "We offer the best cloud solutions customized to your needs",
+  heroText: "Become a leader in Software Development for IT and Cloud Solutions is our vission.",
 }
+
+export default heroContent
