44d92d5a38450e4e536936f050a6cb4b36df46fa
armanopbringas <bringas.armandop@gmail.com>
2021-12-06 11:59:38 -0600

    fix mobile tailwind classes

---
+++ compoents/services-component/index.tsx
@@ -0,0 +1,31 @@
+import { IServices } from '../../fixtures/services.fixtures'
+import styles from './services.module.scss'
+
+const Services = ({ serviceTitle, serviceSubTitle, itemsDercription, imgServiceUrl }: IServices) => {
+  return (
+    <div className={styles["services-container"]}>
+      <div className={styles.container}>
+        <figure className={styles["services-img-container"]}>
+          <img src={imgServiceUrl} />
+        </figure>
+        <div className={styles["services-text-container"]}>
+          <h1 className={styles["services-title"]}>
+            {serviceTitle}
+          </h1>
+          <h3 className={styles["services-subtitle"]}>
+            {serviceSubTitle}
+          </h3>
+          <ul className={styles["services-items"]}>
+            {itemsDercription.map(item => (
+              <li className={styles["service-item"]}>
+                {item}
+              </li>))
+            }
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
