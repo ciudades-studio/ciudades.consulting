5016eb4ad5a02cc416c2ffa664760f08efa5f0d5
armanopbringas <bringas.armandop@gmail.com>
2021-12-07 17:46:52 -0600

    add font sizing and wight styling

--- compoents/contact-us/index.tsx
+++ compoents/contact-us/index.tsx
@@ -15,14 +15,21 @@ export const TitleContainer = ({ contactUsTtitle, contactUsSubtitle }: IContactU
 export const ContactForm = () => {
   return (
     <div className={styles["form-container"]}>
-      <div className={styles["form"]}>
-        <h2 className={styles["form-title"]}>Contact Us</h2>
-      </div>
+      <h2 className={styles["form-title"]}>Contact Us</h2>
+      <form className={styles["form"]}>
+        <label className={styles["form-label"]} htmlFor="/">Name</label>
+        <input className={styles["form-input"]} type="text" placeholder="Enter your full name" />
+        <label className={styles["form-label"]} htmlFor="/">Email</label>
+        <input className={styles["form-input"]} type="text" placeholder="Enter your email" />
+        <label className={styles["form-label"]} htmlFor="/">Message</label>
+        <textarea className={styles["form-input"]} placeholder="Tell us about your project" />
+        <button className={styles["form-button"]}>Send Message</button>
+      </form>
     </div>
   );
 }
 
-const ContactUs = ({ contactUsTtitle, contactUsSubtitle }: IContactUs,) => {
+const ContactUs = ({ contactUsTtitle, contactUsSubtitle }: IContactUs) => {
   return (
     <div className={styles["contactus-container"]}>
       <TitleContainer contactUsTtitle={contactUsTtitle} contactUsSubtitle={contactUsSubtitle} />
