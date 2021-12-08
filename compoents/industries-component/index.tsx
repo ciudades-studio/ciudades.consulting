193e68877139137a17da190fda6d8fc40e689662
armanopbringas <bringas.armandop@gmail.com>
2021-12-08 12:23:24 -0600

    trying add images

--- compoents/industries-component/index.tsx
+++ compoents/industries-component/index.tsx
@@ -1,12 +1,14 @@
 import { IIndustries } from '../../fixtures/industries.fixture'
+import Image from 'next/image'
 import styles from './industries.module.scss'
- 
+
+
 const Industries = ({ industriesTitle, industriesDescription, industrieImgUrl }: IIndustries) => {
   return (
     <div className={styles["industries-container"]}>
       <div className={styles.container}>
         <figure className={styles["industries-image-container"]}>
-          <img src={industrieImgUrl} alt="image" />
+          <Image src={industrieImgUrl} alt="image" width={"333px"} height={"239px"} />
         </figure>
         <div className={styles["industries-text-container"]}>
           <h1>{industriesTitle}</h1>
