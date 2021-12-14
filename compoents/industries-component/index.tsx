78550ab51e25d0743eaf9270c35e9cd6bd673607
armanopbringas <bringas.armandop@gmail.com>
2021-12-14 10:49:20 -0600

    import image as module

--- compoents/industries-component/index.tsx
+++ compoents/industries-component/index.tsx
@@ -2,12 +2,14 @@ import { IIndustries } from '../../fixtures/industries.fixture'
 import Image from 'next/image'
 import styles from './industries.module.scss'
 
-const Industries = ({ industriesTitle, industriesDescription, industrieImgUrl }: IIndustries) => {
+import imageExample from '../../assets/industries-assets/industries01.png'
+
+const Industries = ({ industriesTitle, industriesDescription }: IIndustries) => {
   return (
     <div className={styles["industries-container"]}>
       <div className={styles.container}>
         <figure className={styles["industries-image-container"]}>
-          <Image src={industrieImgUrl} alt="image" width={"333px"} height={"239px"} />
+          <Image src={imageExample} alt="image" width={"333px"} height={"239px"} />
         </figure>
         <div className={styles["industries-text-container"]}>
           <h1 className={styles["industries-text-title"]}>{industriesTitle}</h1>
