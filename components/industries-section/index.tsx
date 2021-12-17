47e078991f2faca1b1adb317148e64194cceff8b
armanopbringas <bringas.armandop@gmail.com>
2021-12-16 18:32:36 -0600

    indent code

--- components/industries-section/index.tsx
+++ components/industries-section/index.tsx
@@ -1,12 +1,18 @@
-import Industries from '../industries-component'
+import Industrie from '../industries-component'
 import styles from './industries-section.module.scss'
 import industriesFixtures from '../../fixtures/industries.fixture'
 
 const IndustriesSection = () => {
   return (
     <div className={styles["industries-container"]}>
-      <h2>Industries</h2>
-      {industriesFixtures.map((industrie, index) => <Industries {...industrie} key={index} />)}
+      <h2 className={styles["industries-section-title"]}>
+        Industries
+      </h2>
+      {industriesFixtures.map((industrie, index) => {
+        return (
+            <Industrie {...industrie} key={index} />
+          );
+      })}
     </div>
   );
 }
