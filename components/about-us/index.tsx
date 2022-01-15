3a1826fa41dc5acaafb3498cd5d4a5fc2a9e2be2
Noel Escobedo <noel.escobedo@wizeline.com>
2022-01-14 22:26:22 -0600

    Added about us images and styles

--- components/about-us/index.tsx
+++ components/about-us/index.tsx
@@ -1,5 +1,10 @@
 import { IAboutUs } from '../../fixtures/about-us-fixture'
 import styles from './about-us.module.scss'
+import Points from '../../assets/about-us-assets/points.png'
+import ImgRight1 from '../../assets/about-us-assets/img-right1.jpg'
+import ImgRight2 from '../../assets/about-us-assets/img-right2.jpg'
+import ImgRight3 from '../../assets/about-us-assets/img-right3.jpg'
+import Image from 'next/image'
 
 function AboutUs({ aboutUsTitle, aboutUsDescription }: IAboutUs): JSX.Element {
   return (
@@ -9,6 +14,20 @@ function AboutUs({ aboutUsTitle, aboutUsDescription }: IAboutUs): JSX.Element {
           <h2 className={styles["aboutus-title"]}>{aboutUsTitle}</h2>
           <div className={styles["aboutus-description"]}>
             {aboutUsDescription.map(item => <p>{item}</p>)}
+          </div>
+        </div>
+        <div className={styles["aboutus-images-right"]}>
+          <div className={styles.points}>
+            <Image src={Points} width={126} height={206} />
+          </div>
+          <div className={styles['img-right1']}>
+            <Image src={ImgRight1} width={265} height={265} />
+          </div>
+          <div className={styles['img-right2']}>
+            <Image src={ImgRight2} width={265} height={265}  />
+          </div>
+          <div className={styles['img-right3']}>
+            <Image src={ImgRight3} width={265} height={265}  />
           </div>
         </div>
       </div>
