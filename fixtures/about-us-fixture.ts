66da0971babac42ab9071b835a48a73dcd86a68c
Diego Benjamín Aguilar Aguilar <diegoaguilar@users.noreply.github.com>
2022-01-18 17:13:30 -0600

    Discard egocentric copy

--- fixtures/about-us-fixture.ts
+++ fixtures/about-us-fixture.ts
@@ -5,15 +5,7 @@ export interface IAboutUs {
 
 const aboutUs = {
   aboutUsTitle: "About Us",
-  aboutUsDescription: [
-    "Diego has lived in multiple cities.",
-    "Changes and life experiences have always came along a move from one city to another.",
-    "I’ve noticed Software and Technology some-times is even a motivation and strategy for cities expansion. ",
-    "I read Invisible Cities, by Calvino which motivated me to start on this concept. ",
-    "I love “Las Ciudades”, song by José Alfredo Jiménez, best composer ever.",
-    "Diego moves to Guadalajara (new city, once again) and approaches Omar to start on the project.",
-    "They’re in talks to start a great enterprise."
-  ],
+  aboutUsDescription: [],
 }
 
 export default aboutUs
