ee73c1e9c3ed018985ac33afbd12e5796b520eec
Diego Benjamin Aguilar Aguilar <diegoaguilar@users.noreply.github.com>
2022-05-23 17:17:13 -0500

    Footer added

--- fixtures/about-us-fixture.ts
+++ fixtures/about-us-fixture.ts
@@ -6,7 +6,8 @@ export interface IAboutUs {
 const aboutUs = {
   aboutUsTitle: "About Us",
   aboutUsDescription: [
-    "We're a digital products development & consulting company "
+    "We're a digital products & software engineering company helping projects succeed with consultancy driven approach.",
+    "We're committed to bring innovative and reliable solutions, focusing on best practices in the scope of work.",
   ],
 }
 
